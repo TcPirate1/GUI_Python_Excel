b9405977f19d5ae19c744b3d5bd444edaaf0aeda
TC_windows <88120195+TcPirate1@users.noreply.github.com>
2023-01-19 23:22:18 +1300

    Initializing gitignore and readme files. Able to switch between 3 screens with buttons and clear entries.

---
+++ card_finding_window.py
@@ -0,0 +1,9 @@
+
+
+class CodeSearcher:
+    def CardSearcher():
+        print("Searching for code...")
+
+class NameSearcher:
+    def CardSearcher():
+        print("Searching for name...")
--- starting_window.py
+++ starting_window.py
@@ -1,11 +1,12 @@
 import tkinter as tk
 import tkinter.ttk as ttk
+from card_finding_window import CodeSearcher, NameSearcher
 
 window = tk.Tk() # Window configurations
 window.title("FFTCG card finder")
 window.configure(bg='black')
 
-setAnswer = tk.StringVar()
+setAnswer = tk.StringVar() # Might need to move to prevent circular importing
 
 # Initialize widgets
 class HomeScreen:
@@ -14,7 +15,7 @@ class HomeScreen:
 
 # Functions
     def clear(self):
-        setAnswer.set("")
+        setAnswer.set("") # Might need to move to prevent circular importing
 
     def search(self):
         # Destroying frame
@@ -24,11 +25,11 @@ class HomeScreen:
         self.frame = tk.Frame(window, width= 800, height= 800)
         label = tk.Label(window, text="Enter either a card code or name then click the respective button")
         userInput = tk.Entry(window, width=25, textvariable=setAnswer)
-        cardCode = tk.Button(window, text="Code")
-        cardName = tk.Button(window, text="Card Name")
+        cardCode = tk.Button(window, text="Code", command= CodeSearcher.CardSearcher)
+        cardName = tk.Button(window, text="Card Name", command= NameSearcher.CardSearcher)
         ClearButton = tk.Button(window, text="Clear", command=self.clear)
 
-        label.grid(row=0,column=0, columnspan=3)
+        label.grid(row=0,column=0, columnspan=3, padx=20 , pady=20)
         userInput.grid(row=1, column=0, columnspan=3)
         ClearButton.grid(row=2,column=0)
         cardCode.grid(row=2, column=1)
@@ -37,7 +38,6 @@ class HomeScreen:
     def screen(self):
         self.frame = tk.Frame(window, width= 800, height= 800)
         self.label = tk.Label(window, text= "Welcome to the FFTCG spreadsheet searcher!\n\nNote: This spreadsheet doesn't contain cards from opus 7 or 14 because all of the Commons and Rares were obtained from these sets.\n\nClick one of the buttons to begin.\n\n").grid(row=0,column=1,padx=50 , pady=50)
-        output = tk.Label(textvariable=setAnswer, width=25)
         Search = tk.Button(window, text="Search", command=self.search)
         # AddWorkSheet = tk.Button(window, text="Add worksheet")
         # WorkOnWorkSheet = tk.Button(window, text="Work on worksheet")
@@ -45,8 +45,7 @@ class HomeScreen:
         # ChangeCellValue = tk.Button(window, text="Change cell value")
 
         # Placing widgets on window
-        output.grid(row=1,column=1, columnspan=2)
-        Search.grid(row=3,column=2)
+        Search.grid(row=1,column=1)
         # AddWorkSheet.grid(row=3,column=3)
         # WorkOnWorkSheet.grid(row=3,column=4)
         # ViewWorkSheet.grid(row=3,column=5)
