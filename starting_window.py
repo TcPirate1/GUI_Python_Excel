12dda1ad3fc8356a477e94ee231f6f2ba470df6c
TC_linux <88120195+TcPirate1@users.noreply.github.com>
2023-02-06 10:21:55 +1300

    created two windows and functions rather than a single window and function for both buttons

--- starting_window.py
+++ starting_window.py
@@ -12,27 +12,30 @@ setAnswer = tk.StringVar()
 def clear():
     setAnswer.set("")
 
-def CardSearcher():
+def cardSearchCode():
     cardToFind = setAnswer.get()
     cardNameRegex = re.match('^\d{1,2}-\d{3}[CRHLS]+$', cardToFind)
-    if(cardNameRegex == None):
-        print("This is not a match.")
-    if(cardNameRegex != None):
-        print("This is a match.")
+    print(cardNameRegex)
+    print(cardToFind)
+
+def cardSearchName():
+    pass
         
 # Initialize widgets
 label = tk.Label(window, text= "Welcome to the FFTCG spreadsheet searcher!\n\nNote: This spreadsheet doesn't contain cards from opus 7 or 14 because all of the Commons and Rares were obtained from these sets.\n\nEnter something then click one of the buttons to begin.\n\n")
-userInput = tk.Entry(window, width=25, textvariable=setAnswer)
-cardCode = tk.Button(window, text="Code", command=CardSearcher)
-cardName = tk.Button(window, text="Card Name", command=CardSearcher)
+codeInput = tk.Entry(window, width=25, textvariable=setAnswer)
+cardInput = tk.Entry(window, width=25, textvariable=setAnswer)
+cardCodeButton = tk.Button(window, text="Code", command=cardSearchCode)
+cardNameButton = tk.Button(window, text="Card Name", command=cardSearchName)
 ClearButton = tk.Button(window, text="Clear", command=clear)
 
 # Placing widgets on window
 label.grid(row=0,column=1,padx=50 , pady=50)
-userInput.grid(row=1, column=0, columnspan=3)
-ClearButton.grid(row=2,column=0)
-cardCode.grid(row=2, column=1)
-cardName.grid(row=2, column=2)
+ClearButton.grid(row=3,column=0)
+codeInput.grid(row=1, column=1)
+cardInput.grid(row=1, column=2)
+cardCodeButton.grid(row=3, column=1)
+cardNameButton.grid(row=3, column=2)
 
 
 window.mainloop()
