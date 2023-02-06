e8cabfc56795f8bda6968385a094cda8b4d5c2ba
TC_linux <88120195+TcPirate1@users.noreply.github.com>
2023-02-06 20:23:32 +1300

    Two global StringVar and a single output window below. Not sure why importing non-gui functions not working.

--- starting_window.py
+++ starting_window.py
@@ -1,41 +1,47 @@
 import tkinter as tk
 import tkinter.ttk as ttk
-import re
 
 window = tk.Tk() # Window configurations
 window.title("FFTCG card finder")
 window.configure(bg='black')
 
-setAnswer = tk.StringVar()
+setCodeAnswer = tk.StringVar()
+setNameAnswer = tk.StringVar()
 
 # Functions
 def clear():
-    setAnswer.set("")
+    setCodeAnswer.set("")
+    setNameAnswer.set("")
 
 def cardSearchCode():
-    cardToFind = setAnswer.get()
-    cardNameRegex = re.match('^\d{1,2}-\d{3}[CRHLS]+$', cardToFind)
-    print(cardNameRegex)
+    cardToFind = setCodeAnswer.get()
     print(cardToFind)
+    output.delete(0.0, tk.END)
 
 def cardSearchName():
-    pass
+    cardtoFind = setNameAnswer.get()
+    print(cardtoFind)
+    output.delete(0.0, tk.END)
         
 # Initialize widgets
 label = tk.Label(window, text= "Welcome to the FFTCG spreadsheet searcher!\n\nNote: This spreadsheet doesn't contain cards from opus 7 or 14 because all of the Commons and Rares were obtained from these sets.\n\nEnter something then click one of the buttons to begin.\n\n")
-codeInput = tk.Entry(window, width=25, textvariable=setAnswer)
-cardInput = tk.Entry(window, width=25, textvariable=setAnswer)
+codeInput = tk.Entry(window, width=25, textvariable=setCodeAnswer)
+nameInput = tk.Entry(window, width=25, textvariable=setNameAnswer)
+outputLabel = tk.Label(window, text="The card is at:")
+output = tk.Text(window, width=25, bg="white", wrap="word")
 cardCodeButton = tk.Button(window, text="Code", command=cardSearchCode)
 cardNameButton = tk.Button(window, text="Card Name", command=cardSearchName)
 ClearButton = tk.Button(window, text="Clear", command=clear)
 
 # Placing widgets on window
 label.grid(row=0,column=1,padx=50 , pady=50)
-ClearButton.grid(row=3,column=0)
+ClearButton.grid(row=2,column=0)
 codeInput.grid(row=1, column=1)
-cardInput.grid(row=1, column=2)
-cardCodeButton.grid(row=3, column=1)
-cardNameButton.grid(row=3, column=2)
+nameInput.grid(row=1, column=2)
+outputLabel.grid(row=3,column=1, pady=20)
+output.grid(row=4,column=1)
+cardCodeButton.grid(row=2, column=1)
+cardNameButton.grid(row=2, column=2)
 
 
 window.mainloop()
