d48339758d8f2b6820442bea9e04b99efef91080
TC_windows <88120195+TcPirate1@users.noreply.github.com>
2023-03-10 19:50:03 +1300

    WIP - Still getting error (but different) after using splitext. TypeError: cannot unpack non-iterable NoneType object

--- path.py
+++ path.py
@@ -7,9 +7,9 @@ from tkinter import filedialog
 # SheetNames = File.sheetnames
 
 def select_file():
-    Path = filedialog.askopenfile(mode='r', filetypes=[('All Files', '*.*')], initialdir=os.path.join(os.path.expanduser('~'), 'Documents'))
+    Path = filedialog.askopenfile(mode='r', filetypes=[('Excel Files', '*.xlsx')], initialdir=os.path.join(os.path.expanduser('~'), 'Documents'))
     
-    if Path:
+    if Path and os.path.splitext(Path)[1] == '.xlsx':
         File = openpyxl.load_workbook(Path, data_only=True)
         SheetNames = File.sheetnames
         return File, SheetNames
